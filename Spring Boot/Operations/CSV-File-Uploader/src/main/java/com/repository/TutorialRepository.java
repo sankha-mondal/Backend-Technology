a8ec235b538e07b25c2b928ad622eea8847264c8
Sankha Subhra Mondal <sankha123@gmail.com>
2022-09-06 01:31:29 +0530

    CSV File [ Uploader | Retrieve | Download ]

--- Spring Boot/Operations/CSV-File-Uploader/src/main/java/com/repository/TutorialRepository.java
+++
@@ -1,9 +0,0 @@
-package com.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.entity.Tutorial;
-
-
-
-public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
-}
